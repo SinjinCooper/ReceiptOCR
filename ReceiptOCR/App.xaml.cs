e4551ff61aac4f4ea821cf83bc419c85a956826c
Sinjin Cooper <coopersinjin@gmail.com>
2020-04-02 18:40:16 -0700

    init

---
+++ ReceiptOCR/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace ReceiptOCR
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Check Permissions
+
+        }
+
+        protected override void OnSleep()
+        {
+
+        }
+
+        protected override void OnResume()
+        {
+
+        }
+    }
+}
---
+++ ReceiptOCR/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.Media;
+
+namespace ReceiptOCR
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void BtnStartCamera_Clicked(object sender, System.EventArgs e)
+        {
+            var photo = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions());
+
+            if (photo != null)
+                imgPhoto.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+        }
+    }
+}
